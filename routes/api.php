6b4b5c2479a25720aa8fbca1f00457c662a14270
Mosab <mosababozra3@gmail.com>
2026-04-04 23:24:00 +0300

    added wallet basics CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\WalletController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +14,11 @@ Route::middleware(['auth:sanctum','role:admin'])->group(function(){
 });
 
 Route::middleware('auth:sanctum')->group(function(){
-
+    Route::post('addWallet', [WalletController::class, 'add_wallet']);
+    Route::get('getWallets', [WalletController::class, 'get_wallets']);
+    Route::get('getOneWallet/{id}', [WalletController::class, 'get_one_wallet']);
+    Route::put('updateWallet/{id}', [WalletController::class, 'update_wallet']);
+    Route::delete('delete_wallet/{id}', [WalletController::class, 'delete_wallet']);
 });
 
 Route::prefix('Auth')->group(function(){
