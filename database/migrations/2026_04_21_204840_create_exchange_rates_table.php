f3687b5a5e4f33287d3075a5e85d2ccb2c7b59cf
Mosab <mosababozra3@gmail.com>
2026-04-26 00:02:44 +0300

    create a command to refresh rate from api key and scheduled it to run daily

---
+++ database/migrations/2026_04_21_204840_create_exchange_rates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->string('base_currency',3);
+            $table->string('target_currency',3);
+            $table->decimal('rate',16,5);
+            $table->unique(['base_currency','target_currency']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+};
