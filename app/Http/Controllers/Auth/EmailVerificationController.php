b34af18e4a6cb3ded31ae9deedf10a07360334a2
Mosab <mosababozra3@gmail.com>
2026-09-04 17:20:16 +0300

    added files to a Auth folder

--- app/Http/Controllers/Auth/EmailVerificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Request;
